bc48a611f85bcb03ba78d245e6736f78f1fa1a2b
ztf <superztf@163.com>
2018-12-07 22:45:40 +0800

    delete redundant comment

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,4 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as path from 'path';
 import * as  fs from 'fs';
@@ -10,22 +8,8 @@ import { TSJSToggle, RawPosition, FindFilePos } from './toggle';
 const EXT_TS = ".ts";
 const EXT_JS = ".js";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "tjgogo" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.tjgogo', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-
         if (vscode.window.activeTextEditor) {
             const current = vscode.window.activeTextEditor.document.fileName;
             const ext = path.extname(current);
@@ -53,7 +37,6 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
 
